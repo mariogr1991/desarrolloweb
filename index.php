af83354109be2f242faccc26a0722019aa750bd8
Felix <felixmtzh.fm@gmail.com>
2016-06-26 01:01:11 -0500

    se agrego el archivo index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>parallax and blur</title>
+	<link rel="stylesheet" href="style/index_style.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src='script/script.js'></script>
+</head>
+<body>
+	<section id='contenedor_general'>
+	</section>
+
+	<section id='body'>
+		<section id='scrolldown'>
+			<p id='titulo'>Scroll Down</p>
+			<p id='menorque'><</p>
+		</section>
+
+		<section id='seccion'>
+		</section>
+	</section>
+</body>
+</html>
